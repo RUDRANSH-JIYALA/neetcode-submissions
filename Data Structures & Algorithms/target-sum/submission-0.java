5da62a9e4b44fb5f384e1096d858ec2d12fbedef
RUDRANSH SINGH JIYALA <rudranshjiyala24@gmail.com>
2026-09-09 08:32:16 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        if (Math.abs(target) > totalSum || (target + totalSum) % 2 != 0) {
+            return 0;
+        }
+
+        int subsetSum = (target + totalSum) / 2;
+        int[] dp = new int[subsetSum + 1];
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int sum = subsetSum; sum >= num; sum--) {
+                dp[sum] += dp[sum - num];
+            }
+        }
+
+        return dp[subsetSum];
+    }
+}
